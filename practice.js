9721f04f7fdb02e376e0f6b4b4498af0afebb826
Withmema <rjsgmldnwn@gmail.com>
2019-12-13 22:33:43 +0900

    Create practice.js

---
+++ practice.js
@@ -0,0 +1,21 @@
+const isthisInfo = {
+  name: "IsthisLee",
+  age: 21,
+  gender: "male",
+  isHandsome: true,
+  favMovies: ["Along the gods", "Transfomer", "Home Alone"],
+  favFood: [
+    {
+      name: "Chicken",
+      fatty: true
+    },
+    {
+      name: "Salad",
+      fatty: false
+    }
+  ]
+};
+
+isthisInfo.gender = "female";
+
+console.log(isthisInfo);
