e19a32e6f474a449b1e34dd60be99ee0adaa8df7
Withmema <rjsgmldnwn@gmail.com>
2019-12-19 16:28:31 +0900

    Update practice.js

--- practice.js
+++ practice.js
@@ -2,7 +2,14 @@ alert("nooooo");
 
 function sayGoodbye() {
   console.log("Goodbye!");
-}
+};
+
+const isthisInfo = {
+  name = "LeeGeonhee",
+  age = "20"
+};
+
+insthisInfo.age = "21";
 
 sayGoodbye();
 console.log();
